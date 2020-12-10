0567d2bef38c35c5bb60eb3b462071d8705ba977
David <david.grech26@gmail.com>
2020-12-10 17:07:33 +0000

    Update script.js

---
+++ quiz-app/script.js
@@ -0,0 +1,31 @@
+const quizData = [
+    {
+        question: 'What is the capital of Indonesia?',
+        a: 'Jakarta',
+        b: 'Medan',
+        c: 'Yogyakarta',
+        d: 'Denpasar',
+        correct: 'a'
+    }, {
+        question: 'What is the population of Indonesia?',
+        a: '100K - 150K',
+        b: '151K - 200K',
+        c: '201K - 250K',
+        d: '251K - 300K',
+        correct: 'd'
+    }, {
+        question: 'How many volcanoes are there in Indonesia?',
+        a: '0 - 50',
+        b: '51 - 100',
+        c: '101 - 150',
+        d: '151 - 200',
+        correct: 'd'
+    }, {
+        question: 'How many islands are there in Indonesia?',
+        a: '1 - 5K',
+        b: '6K - 10K',
+        c: '11K - 15K',
+        d: '16K - 20K',
+        correct: 'd'
+    }
+]
