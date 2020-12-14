cef74bd272830e0ae2a49b7e450128a9b86c4e5b
David <david.grech26@gmail.com>
2020-12-14 18:06:31 +0000

    Update script.js

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -30,6 +30,12 @@ const quizData = [
     }
 ];
 
+const questionEL = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+
 let currentQuestion = 0;
 
 loadQuiz();
